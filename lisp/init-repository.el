6ccc5eec4f8d7d77c4319ac8a3707f88be4b41ce
ukari <muromi.ukari@gmail.com>
2017-11-20 20:51:13 +0900

    remove useless repo

--- lisp/init-repository.el
+++ lisp/init-repository.el
@@ -2,6 +2,5 @@
 
 (add-to-list 'package-archives '("marmalade" . "https://marmalade-repo.org/packages/"))
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
-(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t) ; Org-mode's repository
 
 (provide 'init-repository)
