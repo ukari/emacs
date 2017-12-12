118be734e66789d1557ea70a3a7822926f9fb258
ukari <muromi.ukari@gmail.com>
2017-12-13 00:47:06 +0900

    enable indent-tabs-mode; remove useless code

--- lisp/init-javascript.el
+++ lisp/init-javascript.el
@@ -1,5 +1,17 @@
-(add-hook 'js-mode-hook (lambda () (message "init javascript finished")))
+(require-package 'tern)
+(require-package 'tern-auto-complete)
+
 (add-to-list 'auto-mode-alist '("\\.js\\'" . rjsx-mode))
+
+(add-hook 'rjsx-mode-hook
+          (lambda ()
+            (setq indent-tabs-mode nil)
+	    (setq js2-basic-offset 2)
+	    (setq js2-strict-missing-semi-warning nil)
+	    (auto-complete-mode)
+	    (tern-mode)
+	    (tern-ac-setup)
+	    (message "js mode")))
 
 
 (provide 'init-javascript)
