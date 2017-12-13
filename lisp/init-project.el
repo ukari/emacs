bd1ed42688f99850733be2b678746d8abfe4a9cf
ukari <muromi.ukari@gmail.com>
2017-12-13 21:35:54 +0900

    enable auto refresh speedbar

--- lisp/init-project.el
+++ lisp/init-project.el
@@ -2,7 +2,7 @@
 (setq sr-speedbar-right-side nil)
 (setq sr-speedbar-width 32)
 (setq dframe-update-speed 1)
-(sr-speedbar-refresh-turn-on)
+(setq sr-speedbar-auto-refresh t)
 
 (global-set-key (kbd "<f5>") (lambda()
           (interactive)
