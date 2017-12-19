49cd6c803f48d75cebb20ea801000d2fa77ec463
ukari <muromi.ukari@gmail.com>
2017-12-20 01:45:50 +0900

    better <F4> kill-other-windows support for project feature

--- lisp/init-project.el
+++ lisp/init-project.el
@@ -9,23 +9,37 @@
 
 (setq speedbar-use-images nil)
 
-(defun save-and-kill-other-buffers ()
-  (interactive)
-  (mapc (lambda (b) (if (buffer-file-name b)
-			(with-current-buffer b
-			  (save-buffer b))))
-	(delq (current-buffer) (buffer-list)))
-  (mapc (lambda (b) (if (buffer-file-name b)
-			(kill-buffer b)))
-	(delq (current-buffer) (buffer-list))))
+(defun window-list-without-buffer (buffer-name)
+  (seq-filter (lambda (x) (not (equal buffer-name (buffer-name (window-buffer x))))) (window-list)))
 
-(defun kill-other-windows() (if (not (equal (buffer-name (current-buffer)) "*SPEEDBAR*"))
-				(progn (delete-other-windows)
-				       ;; (let ((current-buffer (current-buffer)))
-				       ;; 	 (sr-speedbar-toggle)
-				       ;; 	 (sr-speedbar-toggle)
-				       ;; 	 (switch-to-buffer current-buffer))
-				       )))
+(defun main-buffer ()
+  (let ((current-buffer (current-buffer)))
+    (if (buffer-file-name current-buffer)
+      current-buffer
+      (window-buffer (car (window-list-without-buffer "*SPEEDBAR*"))))))
+
+(defun map-to-other-buffers (f)
+  (mapc (lambda (b) (if (and (buffer-file-name b)
+			     (not (equal b (main-buffer))))
+			(funcall f b)))
+	(delq (if (buffer-file-name (current-buffer))
+		  (current-buffer)
+		(main-buffer))
+	      (buffer-list))))
+
+(defun save-and-kill-other-buffers ()
+  (interactive) 
+  (let ((main-buffer (main-buffer)))
+    (map-to-other-buffers (lambda (b) (with-current-buffer b (save-buffer b))))
+    (map-to-other-buffers (lambda (b) (kill-buffer b)))
+    (if (not (equal main-buffer (window-buffer (car (window-list-without-buffer "*SPEEDBAR*")))))
+       (progn (sr-speedbar-toggle)
+	      (sr-speedbar-toggle)))))
+
+(defun kill-other-windows()
+  (if (equal (buffer-name (current-buffer)) "*SPEEDBAR*")
+				(switch-to-buffer-other-window (main-buffer)))
+  (delete-other-windows))
 
 (global-set-key (kbd "<f4>") (lambda() (interactive)
 			       (save-and-kill-other-buffers)
