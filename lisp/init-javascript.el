bbec6a2e9ae13f135d7ce867eeb9683a9f0dbc0d
ukari <muromi.ukari@gmail.com>
2018-01-03 04:37:03 +0900

    treat .babelrc as json, close js2 warnings

--- lisp/init-javascript.el
+++ lisp/init-javascript.el
@@ -11,6 +11,7 @@
 	    (setq js2-basic-offset 2)
 	    (setq js2-strict-missing-semi-warning nil)
 	    (setq js2-strict-trailing-comma-warning nil)
+	    (setq js2-mode-show-strict-warnings nil)
 	    (paredit-everywhere)
 	    (tern-mode t)
 	    (auto-complete-mode)
--- lisp/init-json.el
+++ lisp/init-json.el
@@ -3,5 +3,6 @@
 (require-package 'json-mode)
 
 (add-to-list 'auto-mode-alist '("\\.tern-project\\'" . json-mode))
+(add-to-list 'auto-mode-alist '("\\.babelrc\\'" . json-mode))
 
 (provide 'init-json)
