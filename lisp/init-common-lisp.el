ad3fed4a628b73db780c17bda3cf8ddc06263edd
ukari <muromi.ukari@gmail.com>
2018-02-07 03:54:55 +0900

    add parentheses support for slime repl

--- lisp/init-common-lisp.el
+++ lisp/init-common-lisp.el
@@ -5,4 +5,13 @@
 (setq inferior-lisp-program "/usr/local/bin/sbcl")
 (setq slime-contribs '(slime-fancy))
 
+;; from: "ParEdit and SLIME REPL" in https://www.emacswiki.org/emacs/ParEdit
+;; Stop SLIME's REPL from grabbing DEL,
+;; which is annoying when backspacing over a '('
+(defun override-slime-repl-bindings-with-paredit ()
+   (define-key slime-repl-mode-map
+     (read-kbd-macro paredit-backward-delete-key) nil))
+
+(add-hook 'slime-repl-mode-hook 'override-slime-repl-bindings-with-paredit)
+
 (provide 'init-common-lisp)
--- lisp/init-parentheses.el
+++ lisp/init-parentheses.el
@@ -4,14 +4,16 @@
 (require-package 'rainbow-delimiters)
 
 (autoload 'enable-paredit-mode "paredit" "Turn on pseudo-structural editing of Lisp code." t)
-    (add-hook 'emacs-lisp-mode-hook       #'enable-paredit-mode)
-    (add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
-    (add-hook 'ielm-mode-hook             #'enable-paredit-mode)
-    (add-hook 'lisp-mode-hook             #'enable-paredit-mode)
-    (add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
-    (add-hook 'scheme-mode-hook           #'enable-paredit-mode)
+(add-hook 'emacs-lisp-mode-hook       #'enable-paredit-mode)
+(add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
+(add-hook 'ielm-mode-hook             #'enable-paredit-mode)
+(add-hook 'lisp-mode-hook             #'enable-paredit-mode)
+(add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
+(add-hook 'slime-repl-mode-hook       #'enable-paredit-mode)
+(add-hook 'scheme-mode-hook           #'enable-paredit-mode)
 
-(add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
+(add-hook 'prog-mode-hook             #'rainbow-delimiters-mode)
+(add-hook 'slime-repl-mode-hook       #'rainbow-delimiters-mode)
 
 (defun paredit-everywhere ()
   (interactive)
