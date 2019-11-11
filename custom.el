2ee73f54948995332337269c7fdcddfa023372da
ukari <muromi.ukari@gmail.com>
2019-11-11 19:54:37 +0800

    update

--- custom.el
+++ custom.el
@@ -25,5 +25,5 @@
  '(speedbar-show-unknown-files t)
  '(speedbar-use-images nil)
  '(speedbar-verbosity-level 0)
- '(sr-speedbar-auto-refresh t)
+ '(sr-speedbar-auto-refresh nil)
  '(sr-speedbar-right-side nil))
