8f9dd47ea7cd528983be31523d5227a93aa07a1e
ukari <muromi.ukari@gmail.com>
2019-12-25 05:49:09 +0800

    update

--- lisp/init-project.el
+++ lisp/init-project.el
@@ -14,14 +14,16 @@
 (setq speedbar-show-unknown-files t)
 (setq speedbar-use-images nil)
 
-(defun window-list-without-buffer (buffer-name)
-  (seq-filter (lambda (x) (not (equal buffer-name (buffer-name (window-buffer x))))) (window-list)))
+(defvar *skip-needing-buffers-list* (list "*SPEEDBAR*" "*Ilist*"))
+
+(defun window-list-without-skipped-buffers ()
+  (seq-filter (lambda (x) (not (member (buffer-name (window-buffer x)) *skip-needing-buffers-list*))) (window-list)))
 
 (defun main-buffer ()
   (let ((current-buffer (current-buffer)))
     (if (buffer-file-name current-buffer)
-      current-buffer
-      (window-buffer (car (window-list-without-buffer "*SPEEDBAR*"))))))
+        current-buffer
+      (window-buffer (car (window-list-without-skipped-buffers))))))
 
 (defun map-to-other-buffers (f)
   (mapc (lambda (b) (if (and (buffer-file-name b)
@@ -37,35 +39,21 @@
   (let ((main-buffer (main-buffer)))
     (map-to-other-buffers (lambda (b) (with-current-buffer b (save-buffer b))))
     (map-to-other-buffers (lambda (b) (kill-buffer b)))
-    (if (not (equal main-buffer (window-buffer (car (window-list-without-buffer "*SPEEDBAR*")))))
-       (progn (sr-speedbar-toggle)
-	      (sr-speedbar-toggle)))))
+    (if (not (equal main-buffer (window-buffer (car (window-list-without-skipped-buffers)))))
+        (progn (sr-speedbar-toggle)
+	       (sr-speedbar-toggle)))))
 
 (defun kill-other-windows()
-  (if (equal (buffer-name (current-buffer)) "*SPEEDBAR*")
-				(switch-to-buffer-other-window (main-buffer)))
+  (if (member (buffer-name (current-buffer)) (window-list-without-skipped-buffers))
+      (switch-to-buffer-other-window (main-buffer)))
   (delete-other-windows))
 
 (global-set-key (kbd "<f4>") (lambda() (interactive)
 			       (save-and-kill-other-buffers)
 			       (kill-other-windows)))
 
-;; (defmacro skip-buffer-in-action (buffer-name action)
-;;   (let ((fn (make-symbol "fn")))
-;;     `(progn (setq ,fn (symbol-function (quote ,action)))
-;; 	    (defun ,action ()
-;; 	      (interactive)
-;; 	      (funcall ,fn)
-;; 	      (if (equal (buffer-name (current-buffer)) ,buffer-name)
-;; 		  (funcall ,fn))))))
-;; (on-init (lambda ()
-;; 	   (skip-buffer-in-action "*SPEEDBAR*" previous-buffer)
-;; 	   (skip-buffer-in-action "*SPEEDBAR*" next-buffer)))
-
 (advice-add #'previous-buffer :around #'skip-specific-buffers)
 (advice-add #'next-buffer :around #'skip-specific-buffers)
-
-(defvar *skip-needing-buffers-list* (list "*SPEEDBAR*" "*Ilist*"))
 
 (defun skip-specific-buffers (origin &rest rest)
   (let ((start-buffer (current-buffer)))
@@ -84,28 +72,8 @@
 
 (setq pop-up-windows nil)
 
-;; (advice-add #'pop-to-buffer :around #'prevent-split)
-
-;; (defun prevent-split (origin &rest rest)
-;;   (interactive)
-;;   (let ((pop-up-windows nil))
-;;     (apply origin rest)))
-
 (defun main-window ()
   (let ((buffer (main-buffer)))
     (car (seq-filter (lambda (x) (equal buffer (window-buffer x))) (window-list)))))
 
-(defun test ()
-  (interactive)
-  (find-file (buffer-file-name (main-buffer)))
-  (message (buffer-file-name (main-buffer)))
-  (message "access buffer 2"))
-
-;(add-hook 'speedbar-visiting-file-hook (lambda () (test)))
-
-;(remove-hook 'speedbar-before-visiting-file-hook 'sr-speedbar-before-visiting-file-hook)
-;(remove-hook 'speedbar-before-visiting-tag-hook 'sr-speedbar-before-visiting-tag-hook)
-;; todo
-(remove-hook 'speedbar-visiting-file-hook 'sr-speedbar-visiting-file-hook)
-;(remove-hook 'speedbar-visiting-tag-hook 'sr-speedbar-visiting-tag-hook)
 (provide 'init-project)
