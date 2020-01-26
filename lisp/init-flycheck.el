9d8a550f0db26874e77e2ecd5a6007af08c82886
ukari <muromi.ukari@gmail.com>
2020-01-27 00:03:05 +0800

    flycheck

--- lisp/init-flycheck.el
+++ lisp/init-flycheck.el
@@ -2,7 +2,8 @@
 
 (require-package 'flycheck)
 
-(global-flycheck-mode)
 (setq-default flycheck-disabled-checkers '(emacs-lisp-checkdoc))
+;; avoid annoying pop up
+(setq flycheck-display-errors-function nil)
 
 (provide 'init-flycheck)
