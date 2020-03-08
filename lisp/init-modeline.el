80ca736a8564689a3e79e0b0efab38b95d23b926
ukari <muromi.ukari@gmail.com>
2020-03-08 08:58:33 +0800

    pretty modeline

--- lisp/init-modeline.el
+++ lisp/init-modeline.el
@@ -8,8 +8,10 @@
 
 (column-number-mode 1)
 
-(set-face-attribute 'mode-line-buffer-id nil :foreground "#f0c674")
+(set-face-attribute 'mode-line-buffer-id nil :weight 'normal :foreground "#f0c674")
 
-(set-face-foreground 'mode-line "#b0b287")
+(set-face-attribute 'mode-line nil :box nil :foreground "#b0b287" :background "#2d4e36")
+
+(set-face-attribute 'mode-line-inactive nil :box nil)
 
 (provide 'init-modeline)
