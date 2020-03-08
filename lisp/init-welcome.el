ae1f80dc6b2265e099ec2ae04bd50a58bc11ee5c
ukari <muromi.ukari@gmail.com>
2020-03-08 18:13:46 +0800

    support echo area anime

--- lisp/init-welcome.el
+++ lisp/init-welcome.el
@@ -7,6 +7,20 @@
 (defun server-execute-echo-area-silencer (origin &rest rest)
   (let ((inhibit-message t))
     (apply origin rest))
-  (message welcome-message))
+  (type-message welcome-message))
+
+(defun type-message (mes &optional delay)
+  (unless delay
+    (setq delay 0.01))
+  (let ((message-log-max nil)
+        (acc))
+    (seq-map
+     (lambda (char)
+       (setf acc (concat acc char))
+       (sit-for delay)
+       (message acc))
+     (split-string mes "")))
+  (sit-for delay)
+  (message mes))
 
 (provide 'init-welcome)
