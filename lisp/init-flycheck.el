6dc9afeea290f03501c91c00dd2e2d126d531347
ukari <muromi.ukari@gmail.com>
2020-08-31 15:10:21 +0800

    add flycheck-package

--- lisp/init-flycheck.el
+++ lisp/init-flycheck.el
@@ -1,6 +1,7 @@
 ;; -*- lexical-binding: t -*-
 
 (require-package 'flycheck)
+(require-package 'flycheck-package)
 
 (setq-default flycheck-disabled-checkers '(emacs-lisp-checkdoc))
 ;; avoid annoying pop up
@@ -9,5 +10,7 @@
 (defun flycheck-compile-without-interactive ()
   (interactive)
   (flycheck-compile (flycheck-get-checker-for-buffer)))
+
+(flycheck-package-setup)
 
 (provide 'init-flycheck)
