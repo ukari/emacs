ce00bb039ef212b84fad104217526696ed6b4060
ukari <muromi.ukari@gmail.com>
2020-09-01 13:30:17 +0800

    update for charset

--- lisp/init-editor.el
+++ lisp/init-editor.el
@@ -3,7 +3,9 @@
 (setq-default indent-tabs-mode nil)
 
 ;; charset
-(set-language-environment "UTF-8")
+;; (set-language-environment "UTF-8")
+;; (prefer-coding-system 'gb18030)
+;; (prefer-coding-system 'utf-8)
 
 (on-init
  ;;default directory
