fca480aa3a683c262cb7a2d0bafa2afb58bf024f
ukari <muromi.ukari@gmail.com>
2021-02-04 07:50:36 +0800

    update for global rainbow-mode

--- lisp/init-color-visualizing.el
+++ lisp/init-color-visualizing.el
@@ -3,7 +3,9 @@
 (require-package 'rainbow-mode)
 
 (define-globalized-minor-mode global-rainbow-mode rainbow-mode
-  (lambda () (unless (eq major-mode 'speedbar-mode)
+  (lambda () (unless (or (eq major-mode 'speedbar-mode)
+                         (eq major-mode 'imenu-list-major-mode)
+                         (eq major-mode 'glsl-mode))
                (rainbow-mode 1))))
 
 (global-rainbow-mode 1)
