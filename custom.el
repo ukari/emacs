4181cc1b77958f41f85a807f81e17ec009ff0682
chendianbuji <chendianbuji@gmail.com>
2021-04-15 23:42:04 +0800

    auto clear custom-safe-themes

--- custom.el
+++ custom.el
@@ -10,7 +10,6 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-raised-buttons nil)
- '(custom-safe-themes
-   '("ab0596c436f76712bbf7de18935c4490d95c332ed4fc58d435e7bdb9c5650e27" default))
+ '(custom-safe-themes nil)
  '(package-selected-packages
    '(yaml-mode unicad sr-speedbar speedbar-git-respect slime rainbow-mode rainbow-delimiters paredit package-lint olivetti nix-mode markdown-mode json-mode imenu-list haskell-mode glsl-mode fountain-mode flycheck-package flycheck-haskell flycheck exec-path-from-shell diminish diff-hl)))
--- lisp/init-theme.el
+++ lisp/init-theme.el
@@ -1,9 +1,11 @@
 ;; -*- lexical-binding: t -*-
 
-;; (require-package 'monokai-theme)
-
-;;(on-init (load-theme 'monokai t))
-
 (on-init (load-theme 'ground t))
+
+(on-init
+ (message "custom variable custom-safe-themes check finished")
+ (unless custom-safe-themes
+           (customize-save-variable 'custom-safe-themes nil)
+           (message "clear custom-safe-themes")))
 
 (provide 'init-theme)
