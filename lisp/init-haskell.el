47f2fdfb1ce60de4c98d20fed815f2626172194c
chendianbuji <chendianbuji@gmail.com>
2021-04-18 05:11:17 +0800

    temporary patch for comment parse in interactive-haskell mode. [ref](https://github.com/haskell/haskell-mode/commit/4a87d7258936147180a2243fb23aada1c04da240)

--- lisp/init-haskell.el
+++ lisp/init-haskell.el
@@ -15,11 +15,14 @@
 
 (setq haskell-process-path-ghci "stack")
 (setq haskell-process-args-ghci '("ghci"))
+(setq haskell-compile-cabal-build-command "cabal new-build --ghc-option=-ferror-spans")
+(setq haskell-compile-cabal-build-alt-command "cabal new-clean -s && cabal new-build --ghc-option=-ferror-spans")
 
 (advice-add #'haskell-process-load-file :around #'load-haskell-context-wrapper)
 
 (defun load-haskell-context-wrapper (origin &rest rest)
-  (window-manager (lambda () (apply origin rest))))
+  (window-manager (lambda () (apply origin rest)))
+  (modify-syntax-entry ?-  ". 123" haskell-mode-syntax-table))
 
 ;; revert buffer when haskell process reload due to cabal file changed
 ;; https://github.com/haskell/haskell-mode/blob/e72677668f5fc7cc148008e885a0f256e245dd43/haskell-load.el#L33
