8850fca14a41015c6ad90dc21517dbbc54789b69
chendianbuji <chendianbuji@gmail.com>
2021-06-21 18:41:09 +0800

    add init-on

--- lisp/init-utils.el
+++ lisp/init-utils.el
@@ -1,4 +1,5 @@
 ;; -*- lexical-binding: t -*-
+(require 'cl-lib)
 
 (defvar *on-init-list* nil)
 
@@ -8,10 +9,17 @@
 (defun call-on-init ()
   (seq-map (lambda (x) (add-hook 'after-init-hook x)) *on-init-list*))
 
-(defun once(fn)
+(defun once (fn)
   (let ((done nil))
     (lambda () (unless done
                  (funcall fn)
                  (setf done t)))))
+
+(defun once-on (symbol fn)
+  (cl-labels ((initer (origin &rest rest)
+                     (unwind-protect (apply origin rest)
+                       (funcall fn)
+                       (advice-remove symbol #'initer))))
+    (advice-add symbol :around #'initer)))
 
 (provide 'init-utils)
